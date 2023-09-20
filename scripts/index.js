29a445dc544675fc283522e11b79f84169387758
unknown <alfonsoreviejovalle@alumnos.tuniversformacion.es>
2023-09-20 09:13:43 +0200

    Inserccion

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,21 @@
+// Igualamos el id de dentro del parentesis a cambiocolor
 const cambiacolor = document.getElementById('backgr');
 
-cambiacolor.addEventListener('click',()=> {
-    document.body.style.backgroundColor=getRandomColor();
+
+// Funcion para cambiar el codigo en el body y que salga ese valor en el terminal
+cambiacolor.addEventListener('click', () => {
+    document.body.style.backgroundColor = getRandomColor();
+    console.log('El color generado es ' + getRandomColor());
 });
 
 
-function getRandomColor(){
-    const letters='0123456789ABCDEF';
-    let color='#';
+// Funcion para generar un color aleatorio al body
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
 
-    for(let i=0;i<6;i++){
-        color+=letters(Math.floor(Math.random()*16));
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
 
     return color;
